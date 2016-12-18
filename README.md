66b46b56025f5f0dce085ebed1e9f2f04cc067a1
aldraco <a.l.drake713@gmail.com>
2016-12-17 23:22:59 -0500

    Update readme with pip install note

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This project aims to be a simple adapter for batch uploads from data/log processing scripts into common graphing software.
 
+## Installation
+
+~~~~~
+pip install logu
+~~~~~
+
 
 Many stats database/visualization stacks, like statsd/Graphite, only accept statistics in realtime, making uploading historical statistical data somewhat more complicated. Graphite has a bulk upload feature that allows the user to bypass the statsd server, which simply requires setting up a socket and sending pickled data through it.
 
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ setup(
   name = 'logu',
   packages = ['logu'],
   version = '0.1',
-  description = 'Small utility to simplify uploading historical logs to Graphite.',
+  description = 'Small utility to simplify sending historical logs and data to Graphite.',
   author = 'Ashley Drake',
   author_email = 'a.l.drake713@gmail.com',
   url = 'https://github.com/aldraco/logu',
   download_url = 'https://github.com/aldraco/logu/tarball/0.1',
-  keywords = ['graphite', 'logging', 'socket'],
+  keywords = ['graphite', 'logging', 'socket', 'batch upload'],
   classifiers = [],
 )

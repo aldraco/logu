502fdc5160b8411b9a41ecce16f4538caa96f903
aldraco <a.l.drake713@gmail.com>
2016-12-17 23:03:27 -0500

    Add stuff for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'logu',
+  packages = ['logu'],
+  version = '0.1',
+  description = 'Small utility to simplify uploading historical logs to Graphite.',
+  author = 'Ashley Drake',
+  author_email = 'a.l.drake713@gmail.com',
+  url = 'https://github.com/aldraco/logu',
+  download_url = 'https://github.com/aldraco/logu/tarball/0.1',
+  keywords = ['graphite', 'logging', 'socket'],
+  classifiers = [],
+)
